34b92f0f73a13908c09e81fb581e06232a20082c
Nick Allport <nick.allport@fundapps.co>
2021-04-15 15:50:21 +0100

    Initial Commit

---
+++ {{ cookiecutter.solution_name }}/{{ cookiecutter.project_name }}.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace TestWebApi.Integration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
